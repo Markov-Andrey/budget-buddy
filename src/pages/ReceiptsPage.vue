78398c54a7ae1a89f03d3296346e6f3ae1bd5aa8
Andre Markov <andremarkov@icloud.com>
2024-05-28 15:12:46 +0300

    add dynamic Camera input

--- src/pages/ReceiptsPage.vue
+++ src/pages/ReceiptsPage.vue
@@ -1,13 +1,15 @@
 <template>
   <div>Добавить новый чек</div>
+  <ReceiptInputComponent />
   <TableComponent is-edit />
 </template>
 
 <script>
 import TableComponent from "@/components/table/TableComponent";
+import ReceiptInputComponent from "@/components/inputs/ReceiptInputComponent";
 export default {
   name: "ReceiptsPage",
-  components: {TableComponent}
+  components: {ReceiptInputComponent, TableComponent}
 }
 </script>
 
