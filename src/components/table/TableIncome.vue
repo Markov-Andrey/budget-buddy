3f946c5c2431ab7a2988d0547b43650d4d6644d2
Markov Andrey <andremarkov@icloud.com>
2024-06-16 00:36:19 +0300

    add modal to income

--- src/components/table/TableIncome.vue
+++ src/components/table/TableIncome.vue
@@ -25,7 +25,8 @@
                 <td class="px-6 py-4">{{ getSubcategoryName(item.subcategory_id) }}</td>
                 <td class="px-6 py-4">{{ formatRussianDate(item.created_at) }}</td>
                 <td class="px-6 py-4 flex gap-1">
-                    <div :id="`edit-${item.id}`">
+                    <div :id="`edit-${item.id}`"
+                         @mouseup="openModal(item.id)">
                         <ButtonComponent icon="edit"/>
                     </div>
                     <div @mouseup="deleteItem(item.id)">
@@ -34,6 +35,52 @@
                 </td>
             </tr>
             </tbody>
+
+            <!-- Единое модальное окно -->
+            <!-- Выведи в компонент модалок, сделай инпуты, отправку формы, и клик вне формы -->
+            <div v-if="selectedItem" aria-hidden="true" class="fixed top-0 right-0 bottom-0 left-0 flex items-center justify-center z-50 bg-gray-900 bg-opacity-50">
+                <div class="relative p-4 w-full max-w-2xl max-h-full">
+                    <!-- Modal content -->
+                    <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                        <!-- Modal header -->
+                        <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t">
+                            <h3 class="text-xl font-semibold text-gray-900">
+                                Изменить запись
+                            </h3>
+                            <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center"
+                                    @click="selectedItem = null">
+                                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                                </svg>
+                                <span class="sr-only">Close modal</span>
+                            </button>
+                        </div>
+                        <!-- Modal body -->
+                        <div class="p-4 md:p-5 space-y-4">
+                            <p class="text-base leading-relaxed text-gray-500">
+                                {{ selectedItem.amount }}
+                            </p>
+                            <p class="text-base leading-relaxed text-gray-500">
+                                {{ getSubcategoryName(selectedItem.subcategory_id) }}
+                            </p>
+                            <p class="text-base leading-relaxed text-gray-500">
+                                {{ formatRussianDate(selectedItem.created_at) }}
+                            </p>
+                        </div>
+                        <!-- Modal footer -->
+                        <div class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
+                            <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+                                I accept
+                            </button>
+                            <button type="button" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10">
+                                Decline
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
         </table>
 
         <!-- Кнопка для загрузки следующей страницы -->
@@ -60,6 +107,7 @@ export default {
             totalItems: null,
             perPage: 5,
             deleteItems: [],
+            selectedItem: null,
         };
     },
     computed: {
@@ -68,6 +116,10 @@ export default {
         }
     },
     methods: {
+        openModal(id) {
+            this.selectedItem = this.incomeData.find(item => item.id === id);
+            console.log(this.selectedItem);
+        },
         IncomeAdded() {
             this.getIncome();
         },
