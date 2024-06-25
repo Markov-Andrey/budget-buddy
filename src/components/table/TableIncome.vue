6b76922beba6e43e3b04142fa81ac9498a6fbbd1
Andre Markov <andremarkov@icloud.com>
2024-06-25 11:12:09 +0300

    add update and delete method via modal

--- src/components/table/TableIncome.vue
+++ src/components/table/TableIncome.vue
@@ -29,16 +29,17 @@
                          @mouseup="openModal(item.id)">
                         <ButtonComponent icon="edit"/>
                     </div>
-                    <div @mouseup="deleteItem(item.id)">
+                    <div :id="`delete-${item.id}`"
+                         @mouseup="openModalDelete(item.id)">
                         <ButtonComponent icon="delete"/>
                     </div>
                 </td>
             </tr>
             </tbody>
 
-            <!-- Единое модальное окно -->
+            <!-- Модальное окно редактирование -->
             <!-- Выведи в компонент модалок, сделай инпуты, отправку формы, и клик вне формы -->
-            <div v-if="selectedItem" aria-hidden="true" class="fixed top-0 right-0 bottom-0 left-0 flex items-center justify-center z-50 bg-gray-900 bg-opacity-50">
+            <div v-if="selectedItemChange" aria-hidden="true" class="fixed top-0 right-0 bottom-0 left-0 flex items-center justify-center z-50 bg-gray-900 bg-opacity-50">
                 <div class="relative p-4 w-full max-w-2xl max-h-full">
                     <!-- Modal content -->
                     <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
@@ -48,7 +49,70 @@
                                 Изменить запись
                             </h3>
                             <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center"
-                                    @click="selectedItem = null">
+                                    @click="selectedItemChange = null">
+                                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                                </svg>
+                                <span class="sr-only">Close modal</span>
+                            </button>
+                        </div>
+                        <!-- Modal body -->
+                        <div class="p-4 md:p-5 space-y-4">
+                            <div>
+                                <label for="amount" class="block text-base font-medium text-gray-700">Сумма</label>
+                                <input type="text"
+                                       id="amount"
+                                       name="amount"
+                                       class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                       v-model="selectedItemChange.amount"
+                                >
+                            </div>
+                            <div>
+                                <label for="subcategory" class="block text-base font-medium text-gray-700">Подкатегория</label>
+                                <select id="subcategory" v-model="selectedItemChange.subcategory_id" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+                                    <option v-for="subcategory in subcategories" :key="subcategory.id" :value="subcategory.id">
+                                        {{ subcategory.name }}
+                                    </option>
+                                </select>
+                            </div>
+                            <div>
+                                <label for="created_at" class="block text-base font-medium text-gray-700">Дата</label>
+                                <input type="date"
+                                       id="created_at"
+                                       name="created_at"
+                                       class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                       v-model="formattedDate"
+                                >
+                            </div>
+                        </div>
+                        <!-- Modal footer -->
+                        <div class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
+                            <button type="button" @mouseup="update()"
+                                    class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+                                Подтвердить
+                            </button>
+                            <button type="button" @mouseup="selectedItemChange = null"
+                                    class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10">
+                                Отменить
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Модальное окно удаление -->
+            <!-- Выведи в компонент модалок, сделай инпуты, отправку формы, и клик вне формы -->
+            <div v-if="selectedItemDelete" aria-hidden="true" class="fixed top-0 right-0 bottom-0 left-0 flex items-center justify-center z-50 bg-gray-900 bg-opacity-50">
+                <div class="relative p-4 w-full max-w-2xl max-h-full">
+                    <!-- Modal content -->
+                    <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                        <!-- Modal header -->
+                        <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t">
+                            <h3 class="text-xl font-semibold text-gray-900">
+                                Удалить запись
+                            </h3>
+                            <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center"
+                                    @click="selectedItemDelete = null">
                                 <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
                                 </svg>
@@ -58,28 +122,29 @@
                         <!-- Modal body -->
                         <div class="p-4 md:p-5 space-y-4">
                             <p class="text-base leading-relaxed text-gray-500">
-                                {{ selectedItem.amount }}
+                                {{ selectedItemDelete.amount }}
                             </p>
                             <p class="text-base leading-relaxed text-gray-500">
-                                {{ getSubcategoryName(selectedItem.subcategory_id) }}
+                                {{ getSubcategoryName(selectedItemDelete.subcategory_id) }}
                             </p>
                             <p class="text-base leading-relaxed text-gray-500">
-                                {{ formatRussianDate(selectedItem.created_at) }}
+                                {{ formatRussianDate(selectedItemDelete.created_at) }}
                             </p>
                         </div>
                         <!-- Modal footer -->
                         <div class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
-                            <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center">
-                                I accept
+                            <button type="button" @mouseup="deleteItem(selectedItemDelete.id)"
+                                    class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+                                Подтвердить
                             </button>
-                            <button type="button" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10">
-                                Decline
+                            <button type="button" @click="selectedItemDelete = null"
+                                    class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10">
+                                Отмена
                             </button>
                         </div>
                     </div>
                 </div>
             </div>
-
 
         </table>
 
@@ -107,24 +172,52 @@ export default {
             totalItems: null,
             perPage: 5,
             deleteItems: [],
-            selectedItem: null,
+            selectedItemChange: null,
+            selectedItemDelete: null,
         };
     },
     computed: {
         hasNextPage() {
             return this.perPage < this.totalItems;
-        }
+        },
+        formattedDate: {
+            get() {
+                return this.selectedItemChange.created_at.split('T')[0];
+            },
+            set(newDate) {
+                this.selectedItemChange.created_at = newDate + 'T00:00:00.000000Z';
+            }
+        },
     },
     methods: {
         openModal(id) {
-            this.selectedItem = this.incomeData.find(item => item.id === id);
-            console.log(this.selectedItem);
+            this.selectedItemChange = this.incomeData.find(item => item.id === id);
+            console.log(this.selectedItemChange);
+        },
+        openModalDelete(id) {
+            this.selectedItemDelete = this.incomeData.find(item => item.id === id);
+            console.log(this.selectedItemDelete);
         },
         IncomeAdded() {
             this.getIncome();
         },
+        async update() {
+            try {
+                let id = this.selectedItemChange.id;
+                let updatedItem = {
+                    amount: this.selectedItemChange.amount,
+                    created_at: this.selectedItemChange.created_at,
+                    subcategory_id: this.selectedItemChange.subcategory_id
+                };
+                this.selectedItemChange = null;
+                await axiosInstance.put(`income/update/${id}`, updatedItem);
+            } catch (error) {
+                console.error('Error Update:', error);
+            }
+        },
         async deleteItem(item) {
             try {
+                this.selectedItemDelete = null;
                 this.deleteItems.push(item);
                 await axiosInstance.delete(`income/delete/${item}`);
                 await this.getIncome();
@@ -164,11 +257,8 @@ export default {
             const day = date.getDate().toString().padStart(2, '0');
             const month = (date.getMonth() + 1).toString().padStart(2, '0');
             const year = date.getFullYear();
-            const hours = date.getHours().toString().padStart(2, '0');
-            const minutes = date.getMinutes().toString().padStart(2, '0');
-            const seconds = date.getSeconds().toString().padStart(2, '0');
 
-            return `${day}.${month}.${year} ${hours}:${minutes}:${seconds}`;
+            return `${day}.${month}.${year}`;
         }
     },
     mounted() {
